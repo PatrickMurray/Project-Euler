c366d559a880b899a1607e2e68a874a9ac347f5b
Patrick Murray <PatrickMurray@users.noreply.github.com>
2014-12-08 15:19:06 -0500

    Commenting code

--- one.py
+++ one.py
@@ -1,3 +1,31 @@
+"""
+
+Multiples of 3 and 5
+--------------------
+Problem 1
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+Approach
+--------
+Let:
+	a = 3
+	b = 5
+	limit = 1000
+
+Steps:
+1.)	Keep adding "a" while the sum is less than the limit.
+2.)	Repeat step 1 for "b".
+3.) In steps 1 and 2, the common multiples that "a" and "b" share have
+	been accounted for twice. Repeat step 1 with the product of "a" and
+	"b" and subtract the result.
+
+"""
+
 a	= 3
 b	= 5
 limit	= 1000
@@ -13,8 +41,8 @@ def add_to_limit (n, limit):
 	return summation
 
 
-summation =  add_to_limit(a, limit)
-summation += add_to_limit(b, limit)
-summation -= add_to_limit(a * b, limit)
+result =  add_to_limit(a, limit)
+result += add_to_limit(b, limit)
+result -= add_to_limit(a * b, limit)
 
-print summation
+print result
--- three.py
+++ three.py
@@ -1,3 +1,35 @@
+"""
+
+Largest Prime Factor
+--------------------
+Problem 3
+
+The prime factors of 13195 are 5, 7, 13, and 29.
+
+What is the largest prime factor of the number 600851475143?
+
+
+Approach (divide and conquer)
+-----------------------------
+Let:
+	n = 600851475143
+
+Steps:
+1.)	Google & read up on prime numbers.
+2.)	Keep track of the smallest prime number divisible by "n", initially
+	the smallest prime number, two.
+3.)	Loop:
+	a.)	If the number and the smallest prime are divisible, divide the
+		number by the prime and reset the largest prime back to its
+		initial state.
+	b.)	Otherwise, increase the largest prime number by one.
+
+Note:
+As primes are found, the number is broken down into an even smaller
+number.
+
+"""
+
 n = 600851475143
 
 
