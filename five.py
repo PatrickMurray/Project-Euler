1cfdb65ed068ee1f6bb42a0c7f99002093f0ff46
Patrick Murray <PatrickMurray@users.noreply.github.com>
2014-12-09 18:24:10 -0500

    Commenting

--- five.py
+++ five.py
@@ -1,5 +1,33 @@
-start	= 1
-end	= 20
+"""
+
+Smallest Multiple
+-----------------
+Problem 5
+
+2520 is the smallest number that can be divided by each of the numbers
+from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all the
+numbers from 1 to 20?
+
+
+Approach
+--------
+Let:
+	lower	= 1
+	upper	= 20
+
+Steps:
+1.) Find the least common multiple of the lower number and the next
+	number.
+2.)	For the domain (lower, upper], find the least common multiple of
+	each natural number and the result - updating the result each
+	iteration.
+
+"""
+
+lower	= 1
+upper	= 20
 
 
 def least_common_multiple (a, b):
@@ -15,9 +43,9 @@ def greatest_common_divisor (a, b):
 	return a
 
 
-result = start+1
-i = start
-while i <= end:
+result = upper + 1
+i = lower
+while i <= upper:
 	result = least_common_multiple(i, result)
 	i += 1
 	
--- four.py
+++ four.py
@@ -1,4 +1,35 @@
-digits = 3
+"""
+
+Largest Palindrome Product
+--------------------------
+Problem 4
+
+A palindromic number reads the same both ways. The largest palindrome
+made from the product of two 2-digit numbers is 9009 = 91 x 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+Approach
+--------
+Let:
+	digits	= 3
+	lower	= 10 ^ (digits - 1)	= 10^2	= 100
+	upper	= 10 ^ digits		= 10^3	= 1000
+
+Steps:
+1.)	Loop through every number "i" in the domain [lower, upper):
+	a.)	Loop through every number "j" in the domain [i, upper):
+		i.)		Compute the product of "i" and "j"
+		ii.)	If the product is greator than the largest palindrome
+				stored, validate that it is a palindrome - if it is,
+				update the largest.
+
+"""
+
+digits	= 3
+lower	= 10 ** (digits - 1)
+upper	= 10 ** digits
 
 
 def is_palindrome (n):
@@ -20,22 +51,19 @@ def is_palindrome (n):
 
 def largest_palindrome_product (a, b):
 	largest = 0
-	x = a
-	while x < b:
-		y = x
-		while y < b:
-			product = x * y
+	i = a
+	while i < b:
+		j = i
+		while j < b:
+			product = i * j
 			if largest < product and is_palindrome(product):
 				largest = product
-			y += 1
-		x += 1
+			j += 1
+		i += 1
 	
 	return largest
 
 
-minimum = 10 ** (digits - 1)
-maximum = 10 ** digits
-
-print largest_palindrome_product(minimum, maximum)
+print largest_palindrome_product(lower, upper)
 
 
